cf23c260ae0d2cb540ce3b4ee56c9bbb6b7085d2
Paulo Carlos <paulocarlosfilho@gmail.com>
2025-11-07 12:31:19 -0300

    Fix: main.py - Atualizado - Postando

--- dio_blog/main.py
+++ dio_blog/main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
-
+from datetime import datetime, timezone
 
 app = FastAPI()
 
-
-@app.get("/")
-async def read_root():
-    return {"mensagem": "Olá Mundo"}
-
+# A rota deve aceitar o 'framework' como argumento para que o URL funcione
+@app.get("/posts/{framework}")
+async def read_posts(framework: str):
+    # A resposta deve ser uma lista de dicionários [{}, {}]
+    return {
+        "posts": [
+            {
+                'title': f'Criando uma aplicação com {framework}',
+                'date': datetime.now(timezone.utc)
+            },
+            {
+                'title': f'Internalizando uma app {framework}',
+                'date': datetime.now(timezone.utc)
+            }
+        ]
+    }
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
